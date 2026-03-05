62162a30e279638868d30d187fa9c93dd0dbf7b5
kings <161228236+portraitsofkings@users.noreply.github.com>
2026-03-05 19:49:28 +0300

    feat: add more statusline improvements

--- nvim/dot-config/nvim/lua/plugins/heirline.lua
+++ nvim/dot-config/nvim/lua/plugins/heirline.lua
@@ -1,15 +1,27 @@
-  
 return {
     'rebelot/heirline.nvim',
-    -- I don't like this
+    dependencies = {
+        { 'nvim-tree/nvim-web-devicons', opts = {} }
+    },
     config = function()
         local conditions = require('heirline.conditions')
         local utils = require('heirline.utils')
 
-        local ViMode = {
+        local ModeText = {
             init = function(self)
                 self.mode = vim.fn.mode(1)
+                self.modeFirst = self.mode:sub(1, 1)
             end,
+            hl = function(self)
+                return { bg = self.mode_colors[self.modeFirst], fg = 'bg_dark', bold = true, }
+            end,
+            update = {
+                "ModeChanged",
+                pattern = "*:*",
+                callback = vim.schedule_wrap(function()
+                    vim.cmd("redrawstatus")
+                end),
+            },
             static = {
                 mode_names = {
                     n = "N",
@@ -50,9 +62,9 @@ return {
                 mode_colors = {
                     n = "blue" ,
                     i = "green",
-                    v = "cyan",
-                    V =  "cyan",
-                    ["\22"] =  "cyan",
+                    v = "magenta",
+                    V =  "magenta",
+                    ["\22"] =  "magenta",
                     c =  "orange",
                     s =  "purple",
                     S =  "purple",
@@ -63,29 +75,30 @@ return {
                     t =  "red",
                 }
             },
-            provider = function(self)
-                return " %-("..self.mode_names[self.mode].."%) "
-            end,
-            hl = function(self)
-                local mode = self.mode:sub(1, 1) -- get only the first mode character
-                return { bg = self.mode_colors[mode], fg = 'bg_dark', bold = true, }
-            end,
-            update = {
-                "ModeChanged",
-                pattern = "*:*",
-                callback = vim.schedule_wrap(function()
-                    vim.cmd("redrawstatus")
-                end),
+            {
+                provider = '',
+                hl = function(self)
+                    return { fg = self.mode_colors[self.modeFirst], bg = 'bg_dark' }
+                end
+            },
+            {
+                provider = function(self)
+                    return "%-("..self.mode_names[self.modeFirst].."%)"
+                end,
+            },
+            {
+                provider = '',
+                hl = function(self)
+                    return { fg = self.mode_colors[self.modeFirst], bg = 'bg_dark' }
+                end
             },
         }
 
         local FileNameBlock = {
-            -- let's first set up some attributes needed by this component and its children
             init = function(self)
                 self.filename = vim.api.nvim_buf_get_name(0)
             end,
         }
-        -- We can now define some children separately and add them later
 
         local FileIcon = {
             init = function(self)
@@ -102,20 +115,25 @@ return {
         }
 
         local FileName = {
+            condition = function()
+                return vim.bo.filetype ~= 'help'
+            end,
             provider = function(self)
-                -- first, trim the pattern relative to the current directory. For other
-                -- options, see :h filename-modifers
                 local filename = vim.fn.fnamemodify(self.filename, ":.")
                 if filename == "" then return "[No Name]" end
-                -- now, if the filename would occupy more than 1/4th of the available
-                -- space, we trim the file path to its initials
-                -- See Flexible Components section below for dynamic truncation
-                if not conditions.width_percent_below(#filename, 0.25) then
-                    filename = vim.fn.pathshorten(filename)
-                end
                 return filename
             end,
-            hl = { fg = utils.get_highlight("Directory").fg },
+            hl = function()
+                local style = {}
+
+                style.fg = utils.get_highlight("Directory").fg
+
+                if vim.bo.modified then
+                    style.bold = true
+                end
+
+                return style
+            end,
         }
 
         local FileFlags = {
@@ -135,136 +153,88 @@ return {
             },
         }
 
-        -- Now, let's say that we want the filename color to change if the buffer is
-        -- modified. Of course, we could do that directly using the FileName.hl field,
-        -- but we'll see how easy it is to alter existing components using a "modifier"
-        -- component
-
-        local FileNameModifer = {
-            hl = function()
-                if vim.bo.modified then
-                    -- use `force` because we need to override the child's hl foreground
-                    return { fg = "cyan", bold = true, force=true }
-                end
+        local HelpSection = {
+            condition = function()
+                return vim.bo.filetype == 'help'
             end,
+            provider = function()
+                local filename = vim.api.nvim_buf_get_name(0)
+                return vim.fn.fnamemodify(filename, ":t")
+            end,
+            hl = { fg = 'blue' }
         }
 
-        -- let's add the children to our FileNameBlock component
         FileNameBlock = utils.insert(FileNameBlock,
-        FileIcon,
-        utils.insert(FileNameModifer, FileName), -- a new table where FileName is a child of FileNameModifier
-        FileFlags,
-        { provider = '%<'} -- this means that the statusline is cut here when there's not enough space
-    )
+            FileIcon,
+            HelpSection,
+            FileName,
+            FileFlags,
+            -- this means that the statusline is cut here when there's not enough space
+            { provider = '%<'}
+        )
 
-    -- We're getting minimalist here!
-    local Ruler = {
-        -- %l = current line number
-        -- %L = number of lines in the buffer
-        -- %c = column number
-        -- %P = percentage through file of displayed window
-        provider = " %=%(%l/%L%):%-c %P",
-    }
-
-    local LSPActive = {
-        condition = conditions.lsp_attached,
-        update = {'LspAttach', 'LspDetach'},
-
-        -- You can keep it simple,
-        -- provider = " [LSP]",
-
-        -- Or complicate things a bit and get the servers names
-        provider = function()
-            local names = {}
-            for i, server in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
-                table.insert(names, server.name)
-            end
-            return " [" .. table.concat(names, " ") .. "]"
-        end,
-        hl = { fg = "green", bold = true },
-    }
-    local Diagnostics = {
-
-        condition = conditions.has_diagnostics,
-        -- Example of defining custom LSP diagnostic icons, you can copypaste in your config:
-        --vim.diagnostic.config({
-            --  signs = {
-                --    text = {
-                    --      [vim.diagnostic.severity.ERROR] = '',
-                    --      [vim.diagnostic.severity.WARN] = '',
-                    --      [vim.diagnostic.severity.INFO] = '󰋇',
-                    --      [vim.diagnostic.severity.HINT] = '󰌵',
-                    --    },
-                    --  },
-                    --})
-                    -- Fetching custom diagnostic icons
-                    static = {
-                        error_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.ERROR],
-                        warn_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.WARN],
-                        info_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.INFO],
-                        hint_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.HINT],
-                    },
-
-                    -- If you defined custom LSP diagnostics with vim.fn.sign_define(), use this instead
-                    -- Note defining custom LSP diagnostic this way its deprecated, though
-                    --static = {
-                        --    error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
-                        --    warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
-                        --    info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
-                        --    hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
-                        --},
-
-                        init = function(self)
-                            self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
-                            self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
-                            self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
-                            self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
-                        end,
-
-                        update = { "DiagnosticChanged", "BufEnter" },
-
-                        {
-                            provider = "![",
-                        },
-                        {
-                            provider = function(self)
-                                -- 0 is just another output, we can decide to print it or not!
-                                return self.errors > 0 and (self.error_icon .. self.errors .. " ")
-                            end,
-                            hl = { fg = "error" },
-                        },
-                        {
-                            provider = function(self)
-                                return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
-                            end,
-                            hl = { fg = "warning" },
-                        },
-                        {
-                            provider = function(self)
-                                return self.info > 0 and (self.info_icon .. self.info .. " ")
-                            end,
-                            hl = { fg = "info" },
-                        },
-                        {
-                            provider = function(self)
-                                return self.hints > 0 and (self.hint_icon .. self.hints)
-                            end,
-                            hl = { fg = "hint" },
-                        },
-                        {
-                            provider = "]",
-                        },
-                    }
-
-                    -- vim.print(Diagnostics.error_icon, Diagnostics.info_icon, Diagnostics.warn_icon, Diagnostics.hint_icon)
-                    -- Assemble and run
-                    local statusline = { ViMode, FileNameBlock, Diagnostics, LSPActive, Ruler }
-                    local colors = require("tokyonight.colors").setup()
-                    -- colors in opts doesn't work
-                    require('heirline').load_colors(colors)
-                    require('heirline').setup({ statusline = statusline })
+        local Diagnostics = {
+            condition = conditions.has_diagnostics,
+            static = {
+                error_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.ERROR],
+                warn_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.WARN],
+                info_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.INFO],
+                hint_icon = vim.diagnostic.config()['signs']['text'][vim.diagnostic.severity.HINT],
+            },
+            init = function(self)
+                self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+                self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+                self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+                self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+            end,
+            update = { "DiagnosticChanged", "BufEnter" },
+            {
+                provider = " ",
+            },
+            {
+                provider = function(self)
+                    -- 0 is just another output, we can decide to print it or not!
+                    return self.errors > 0 and (self.error_icon .. self.errors .. " ")
                 end,
-                dependencies = {
-                    { 'nvim-tree/nvim-web-devicons', opts = {} }
-                }
-            }
+                hl = { fg = "error" },
+            },
+            {
+                provider = function(self)
+                    return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+                end,
+                hl = { fg = "warning" },
+            },
+            {
+                provider = function(self)
+                    return self.info > 0 and (self.info_icon .. self.info .. " ")
+                end,
+                hl = { fg = "info" },
+            },
+            {
+                provider = function(self)
+                    return self.hints > 0 and (self.hint_icon .. self.hints)
+                end,
+                hl = { fg = "hint" },
+            },
+            {
+                provider = " ",
+            },
+        }
+
+        local Ruler = {
+            -- %l = current line number
+            -- %L = number of lines in the buffer
+            -- %c = column number
+            -- %P = percentage through file of displayed window
+            provider = " %(%l/%L%):%-3c %P",
+        }
+
+        -- load color aliases from the theme
+        local colors = require("tokyonight.colors").setup()
+        require('heirline').load_colors(colors)
+
+        -- assemble the final statusline
+        local statusline = { ModeText, FileNameBlock, Diagnostics, { provider = '%=' }, Ruler }
+        require('heirline').setup({ statusline = statusline })
+    end
+}
