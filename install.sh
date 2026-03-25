a111a897dad81643cdf6bc1079966a25c629d7db
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-03-25 15:35:32 +0300

    refactor: introduce new structure

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Quit immediately on any error
+set -e
+
+# DOTFILES_DIR="$HOME/.dotfiles"
+#
+# for script in "scripts/*"; do
+#     echo $script
+# done
+
+# for package in */; do
+#     echo $package
+# done
