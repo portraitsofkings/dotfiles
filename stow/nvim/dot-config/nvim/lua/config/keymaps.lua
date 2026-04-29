571df2522191830c11c7f11e77c69102ab5fe714
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-04-29 14:57:59 +0300

    feat: assign leader key sooner

--- stow/nvim/dot-config/nvim/lua/config/keymaps.lua
+++ stow/nvim/dot-config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 local function applyKeymaps(maps)
     for _, map in ipairs(maps) do
         vim.keymap.set(map.mode, map.lhs, map.rhs, map.opts)
