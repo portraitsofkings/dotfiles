7b73eaa2375526ff891a06580d64c39241939ddd
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-01 00:06:15 +0300

    feat: add bufferline and scope plugins

---
+++ stow/nvim/dot-config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+  'akinsho/bufferline.nvim',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  opts = {
+    options = {
+      mode = 'tabs',
+      -- Show only when more than 1
+      always_show_bufferline = false,
+    }
+  }
+}
