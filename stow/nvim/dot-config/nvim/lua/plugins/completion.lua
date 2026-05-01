2f45ee56670adb39e622d0c35ce7163688201b66
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-01 10:41:52 +0300

    feat: enable completion auto-insert

--- stow/nvim/dot-config/nvim/lua/plugins/completion.lua
+++ stow/nvim/dot-config/nvim/lua/plugins/completion.lua
@@ -9,7 +9,7 @@ return {
     },
     completion = {
       documentation = { auto_show = true, auto_show_delay_ms = 0 },
-      list = { selection = { preselect = true, auto_insert = false } }
+      list = { selection = { preselect = true, auto_insert = true } }
     },
     sources = {
       default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
@@ -25,7 +25,7 @@ return {
     fuzzy = { implementation = "prefer_rust_with_warning" },
     cmdline = {
       completion = {
-        list = { selection = { preselect = true, auto_insert = false } },
+        list = { selection = { preselect = true, auto_insert = true } },
       }
     },
   },
