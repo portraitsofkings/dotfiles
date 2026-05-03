2e58c4791d9641a1125313f97b2316400590261a
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-03 23:08:26 +0300

    feat: install noice.nvim and its dependencies

--- stow/nvim/dot-config/nvim/lua/plugins/treesitter.lua
+++ stow/nvim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
   build = ':TSUpdate',
   main = "nvim-treesitter.configs",
   opts = {
-    ensure_installed = { "html", "css", "javascript" },
+    ensure_installed = { "html", "css", "javascript", "regex", "bash" },
     auto_install = true,
     highlight = { enable = true },
     incremental_selection = { enable = true },
