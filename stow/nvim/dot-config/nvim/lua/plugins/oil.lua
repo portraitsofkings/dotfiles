01dfd1249aac36a89fc0948960e50ac78a095208
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-03 19:19:37 +0300

    feat: define custom oil keymaps

--- stow/nvim/dot-config/nvim/lua/plugins/oil.lua
+++ stow/nvim/dot-config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,20 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    use_default_keymaps = false,
+    keymaps = {
+      ["<leader>e?"] = { "actions.show_help", mode = "n" },
+      ["<CR>"] = "actions.select",
+      ["<leader>es"] = { "actions.select", opts = { horizontal = true } },
+      ["<leader>ev"] = { "actions.select", opts = { vertical = true } },
+      ["<leader>et"] = { "actions.select", opts = { tab = true } },
+      ["<leader>ec"] = { "actions.close", mode = "n" },
+      ["<leader>eh"] = { "actions.toggle_hidden", mode = "n" },
+      ["<leader>eo"] = { "actions.change_sort", mode = "n" },
+      ["<leader>ep"] = { "actions.preview" },
+    }
+  },
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   lazy = false
 }
