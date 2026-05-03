5cf4b891ab560f8a9b73078fe30260d669968257
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-03 22:44:31 +0300

    fix: update and properly configure treesitter

--- stow/nvim/dot-config/nvim/lua/plugins/treesitter.lua
+++ stow/nvim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,14 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
-  build = ':TSUpdate'
+  branch = 'master',
+  build = ':TSUpdate',
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = { "html", "css", "javascript" },
+    auto_install = true,
+    highlight = { enable = true },
+    incremental_selection = { enable = true },
+    indent = { enable = true },
+  }
 }
