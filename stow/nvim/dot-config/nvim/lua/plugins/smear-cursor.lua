283b82ae68bc50fa189b1ece02f3e743820babe1
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-04 20:34:05 +0300

    fix(nvim): search characters becoming unreadable

--- stow/nvim/dot-config/nvim/lua/plugins/smear-cursor.lua
+++ stow/nvim/dot-config/nvim/lua/plugins/smear-cursor.lua
@@ -1,1 +1,11 @@
-return { "sphamba/smear-cursor.nvim", config = true, }
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    filetypes_disabled = {
+      "noice",
+    },
+    -- Fix unreadable search in noice.nvim command line
+    never_draw_over_target = true,
+    hide_target_hack = true,
+  }
+}
