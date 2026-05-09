9267a9044d706198286e553a0fe8ee36844a4f4d
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-09 16:11:52 +0300

    feat(nvim): add css intellisense for html plugin

---
+++ stow/nvim/dot-config/nvim/lua/plugins/html-css.lua
@@ -0,0 +1,45 @@
+return {
+  "Jezda1337/nvim-html-css",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    enable_on = {
+      "html",
+      "htmldjango",
+      "tsx",
+      "jsx",
+      "erb",
+      "svelte",
+      "vue",
+      "blade",
+      "php",
+      "templ",
+      "astro",
+    },
+    handlers = {
+      definition = {
+        bind = "gd"
+      },
+      hover = {
+        bind = "K",
+        wrap = true,
+        border = "none",
+        position = "cursor",
+      },
+    },
+    documentation = {
+      auto_show = true,
+    },
+    peek = {
+      enabled = true,
+      border = "rounded",
+      position = "center",
+      width = 0.5,
+      height = 0.5,
+      focus = true,
+      style = "minimal",
+    },
+    style_sheets = {},
+  },
+}
