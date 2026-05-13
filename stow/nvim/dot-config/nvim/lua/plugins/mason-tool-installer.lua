44f841b9bbe2f28331df4b00aa79cd5b3c9f7dd1
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-13 11:43:13 +0300

    build(nvim): add dependency entry for safety

--- stow/nvim/dot-config/nvim/lua/plugins/mason-tool-installer.lua
+++ stow/nvim/dot-config/nvim/lua/plugins/mason-tool-installer.lua
@@ -1,5 +1,8 @@
 return {
   "WhoIsSethDaniel/mason-tool-installer.nvim",
+  dependencies = {
+    { "mason-org/mason.nvim", config = true },
+  },
   opts = {
     ensure_installed = { "stylua" },
   },
