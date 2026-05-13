9e51691760f572005882669be50edda7f2d59c16
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-13 13:48:48 +0300

    feat(nvim): add code block split/join keymaps

---
+++ stow/nvim/dot-config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,19 @@
+return {
+  "Wansmer/treesj",
+  keys = {
+    {
+      "<leader>m",
+      desc = "Treesj: Toggle",
+    },
+    {
+      "<leader>j",
+      desc = "Treesj: Join",
+    },
+    {
+      "<leader>s",
+      desc = "Treesj: Split",
+    },
+  },
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = true,
+}
