be7b6384f4e475992c6fd3ac6181b68ee3f461f4
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-15 14:50:10 +0300

    feat(zsh): lazy-load nvm

--- stow/zsh/dot-config/zsh/environment.zsh
+++ stow/zsh/dot-config/zsh/environment.zsh
@@ -3,11 +3,6 @@ export XDG_CONFIG_HOME="$HOME/.config"
 # Use neovim as man pager
 export MANPAGER="nvim +Man!"
 
-# nvm setup
-export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # Where to write history file
 HISTFILE=~/.zsh_history
 # How many commands to keep in memory
--- stow/zsh/dot-config/zsh/plugins.zsh
+++ stow/zsh/dot-config/zsh/plugins.zsh
@@ -11,3 +11,16 @@ ZSH_AUTOSUGGEST_STRATEGY=(completion)
 
 # fzf completion menu
 zinit light Aloxaf/fzf-tab
+
+# ~~~ nvm ~~~
+# Where to install nvm
+export NVM_DIR="$HOME/.config/nvm"
+# Lazy load
+export NVM_LAZY_LOAD=true
+# Enable nvm completions
+export NVM_COMPLETION=true
+# Don't run install or use on cd into directory with .nvmrc
+export NVM_AUTO_USE=false
+# Don't run `nvm use default` automatically
+export NVM_NO_USE=true
+zinit light lukechilds/zsh-nvm
