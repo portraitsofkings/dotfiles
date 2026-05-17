b6683a1d62427ea77d61cf9da9f76391b2077e28
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-17 15:12:47 +0300

    fix(nvim): remove conflicting keymaps

--- stow/nvim/dot-config/nvim/lua/plugins/treesj.lua
+++ stow/nvim/dot-config/nvim/lua/plugins/treesj.lua
@@ -5,14 +5,6 @@ return {
       "<leader>m",
       desc = "Treesj: Toggle",
     },
-    {
-      "<leader>j",
-      desc = "Treesj: Join",
-    },
-    {
-      "<leader>s",
-      desc = "Treesj: Split",
-    },
   },
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = true,
