2010b790529a230715dfa99ae312d111158cbd13
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-19 15:12:57 +0300

    feat(nvim): stop lazy plugin change notifications

--- stow/nvim/dot-config/nvim/lua/config/lazy.lua
+++ stow/nvim/dot-config/nvim/lua/config/lazy.lua
@@ -24,4 +24,5 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "tokyonight" } },
+  change_detection = { notify = false },
 })
