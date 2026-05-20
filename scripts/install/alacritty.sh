3f9ec01be3274366f8ede95ad33a2b0cbf85232e
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-20 12:02:41 +0300

    feat(alacritty): add install script

---
+++ scripts/install/alacritty.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Quit immediately on any error
+set -e
+
+NAME="alacritty"
+  
+RESET='\e[0m'
+RED='\e[0;31m'
+BLUE='\e[0;34m'
+
+if command -v $NAME > /dev/null; then
+    echo -e "${BLUE}$NAME already installed${RESET}"
+    exit 0
+fi
+
+echo -e "${BLUE}Installing $NAME...${RESET}"
+
+case "$INSTALLER" in
+    "dnf")
+        sudo dnf install $NAME -y
+        exit 0
+    ;;
+esac
+
+echo -e "${RED}[ERROR] No supported installer found${RESET}"
+exit 1
+
