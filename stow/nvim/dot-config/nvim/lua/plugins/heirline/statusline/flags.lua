c25ae3b0a9b2fff97c0bfd3b2e579ea90c2bf327
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-07-28 14:04:19 +0300

    feat(nvim): show [New] for buffers without a file

--- stow/nvim/dot-config/nvim/lua/plugins/heirline/statusline/flags.lua
+++ stow/nvim/dot-config/nvim/lua/plugins/heirline/statusline/flags.lua
@@ -1,6 +1,15 @@
 return {
   {
     condition = function()
+      local isNormalBuffer = vim.bo.buftype == ""
+      local hasName = vim.api.nvim_buf_get_name(0) ~= ""
+      return isNormalBuffer and hasName
+    end,
+    { provider = " " },
+    { provider = "[New]" },
+  },
+  {
+    condition = function()
       return vim.bo.modified
     end,
     hl = { fg = "green" },
